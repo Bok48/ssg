ab77b2b8020ecd3f8f4205cab4efd39cb72b561d
Bok48 <janolav.aspelund@lyse.net>
2024-06-25 14:13:01 +0200

    2.4: add leaf node class and tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -11,7 +11,7 @@ class HTMLNode:
         raise NotImplementedError
     
     # This:           {"href": "https://www.google.com", "target": "_blank"}
-    # Should become:  'href="https://www.google.com" target="_blank"'
+    # Should become:  ' href="https://www.google.com" target="_blank"'
     def props_to_html(self):
         html_string = ''
         for prop in self.props:
@@ -21,3 +21,16 @@ class HTMLNode:
     
     def __repr__(self):
         return f'HTMLNode(tag: {self.tag}, value: {self.value}, children: {self.children}, props: {self.props})'
+    
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag=None, value=None, props=""):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("No value set for leaf node")
+        if self.tag is None:
+            return self.value
+        return f'<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>'
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,7 @@
 import unittest
 
 from htmlnode import HTMLNode
+from htmlnode import LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_props_to_html(self):
@@ -24,6 +25,26 @@ class TestHTMLNode(unittest.TestCase):
         # node_list = [HTMLNode("<p>", "This is example text", None, {"a": "exampleA", "b": "exampleB", "foo": "bar"}),
         #              HTMLNode("<h1>", "This is header text", None, {"lol": "nah"})]
         # print(node_list)
+
+class TestLeafNode(unittest.TestCase):
+    def test_to_html(self):
+        # Test 1
+        node = LeafNode("p", "This is a paragraph of text.")
+        result = node.to_html()
+        expected_result = '<p>This is a paragraph of text.</p>'
+        self.assertEqual(result, expected_result)
+
+        # Test 2
+        node2 = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        result2 = node2.to_html()
+        expected_result2 = '<a href="https://www.google.com">Click me!</a>'
+        self.assertEqual(result2, expected_result2)
+
+        # Test 3
+        node3 = LeafNode(None, "Hello there!")
+        result3 = node3.to_html()
+        expected_result3 = "Hello there!"
+        self.assertEqual(result3, expected_result3)
 
 
 if __name__ == "__main__":
