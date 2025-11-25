af3988a23960e01209f7a8f8b1358202003c2c5a
Bok48 <janolav.aspelund@gmail.com>
2025-11-25 19:53:27 +0100

    add: text_to_textnodes function and test

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -9,6 +9,7 @@ from textnode import (
     extract_markdown_links,
     split_nodes_image,
     split_nodes_link,
+    text_to_textnodes,
 )
 
 
@@ -305,6 +306,22 @@ class TestTextNode(unittest.TestCase):
             TextNode(". This is cool.", TextType.TEXT),
         ]
         self.assertEqual(new_nodes, expected_result)
+
+    def test_text_to_textnodes(self):
+        text = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+        result = text_to_textnodes(text)
+        expected_result = [
+            TextNode("This is ", TextType.TEXT),
+            TextNode("text", TextType.BOLD),
+            TextNode(" with an ", TextType.TEXT),
+            TextNode("italic", TextType.ITALIC),
+            TextNode(" word and a ", TextType.TEXT),
+            TextNode("code block", TextType.CODE),
+            TextNode(" and an ", TextType.TEXT),
+            TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+            TextNode(" and a ", TextType.TEXT),
+            TextNode("link", TextType.LINK, "https://boot.dev"),
+        ]
 
 
 if __name__ == "__main__":
--- src/textnode.py
+++ src/textnode.py
@@ -131,3 +131,13 @@ def split_nodes_link(old_nodes):
                     TextNode(node_strings[i], TextType.TEXT)
                 )
     return new_nodes
+
+def text_to_textnodes(text):
+    new_nodes = [TextNode(text, TextType.TEXT)]
+    new_nodes = split_nodes_delimiter(new_nodes, "**", TextType.BOLD)
+    new_nodes = split_nodes_delimiter(new_nodes, "_", TextType.ITALIC)
+    new_nodes = split_nodes_delimiter(new_nodes, "`", TextType.CODE)
+    new_nodes = split_nodes_image(new_nodes)
+    new_nodes = split_nodes_link(new_nodes)
+    return new_nodes
+    
