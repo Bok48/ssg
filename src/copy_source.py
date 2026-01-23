15638c18cb6f754641e6317625a66d73b63ffd0f
Bok48 <janolav.aspelund@gmail.com>
2026-01-23 09:48:07 +0100

    move: copy file functions from main.py to copy_source.py

---
+++ src/copy_source.py
@@ -0,0 +1,23 @@
+import os
+import shutil
+
+def clear_destination_and_copy_to(source, destination):
+    if os.path.exists(destination):
+        shutil.rmtree(destination)
+        print("Public folder cleared")
+    os.mkdir(destination)
+    copy_dir_contents(source, destination)
+    
+def copy_dir_contents(source, destination):
+    if not os.path.exists(source):
+        raise Exception(f"Source folder {source} does not exist in the directory")
+    
+    for item in os.listdir(source):
+        item_path = os.path.join(source, item)
+        dest_path = os.path.join(destination, item)
+        if os.path.isfile(item_path):
+            shutil.copy(item_path, dest_path)
+            print(f"Copied file {item_path} to public folder")
+        else:
+            os.mkdir(dest_path)
+            copy_dir_contents(item_path, dest_path)
