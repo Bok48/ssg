2aa9458e9fd4b96b3c62b1dbd6654e45afa41b5c
Bok48 <janolav.aspelund@gmail.com>
2026-01-23 10:08:02 +0100

    add: extract_title function

---
+++ src/extract_markdown.py
@@ -0,0 +1,12 @@
+from functions.py import (
+    markdown_to_blocks,
+)
+
+def extract_title(markdown):
+    md_blocks = markdown_to_blocks(markdown)
+    for block in md_blocks:
+        print(f"Block: {block}")
+        line_split = block.split(' ', 1)
+        if len(line_split[0]) == 1 and line_split[0] == '#' and len(line_split) > 1 and len(line_split[1]) < 60:
+            return line_split[1]
+    raise Exception("Error: valid markdown h1 header not found. Make sure main header starts with a single '#'")
