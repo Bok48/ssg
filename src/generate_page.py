19a37d5b0a576f693be53fdc014000ca803be36c
Bok48 <janolav.aspelund@gmail.com>
2026-02-06 10:16:27 +0100

    add: generates html files from all md files in tree

--- src/generate_page.py
+++ src/generate_page.py
@@ -7,11 +7,22 @@ from extract_markdown import (
     extract_title,
 )
 
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    source_dir_contents = os.listdir(dir_path_content)
+    for item in source_dir_contents:
+        source_item_path = os.path.join(dir_path_content, item)
+        dest_item_path = os.path.join(dest_dir_path, item)
+        if os.path.isdir(source_item_path):
+            os.mkdir(dest_item_path)
+            generate_pages_recursive(source_item_path, template_path, dest_item_path)
+        else:
+            filepath, extension = os.path.splitext(dest_item_path)
+            if extension == ".md":
+                dest_item_path = f"{filepath}.html"
+            generate_page(source_item_path, template_path, dest_item_path)
 
 def generate_page(from_path, template_path, dest_path):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
-    #if not os.path.exists(from_path) or not os.path.exists(template_path):
-    #    raise Exception(f"Error: file '{from_path}' was not found")
     source_file_contents = ""
     template_file_contents = ""
     with open(from_path) as f:
--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from generate_page import(
+    generate_pages_recursive,
     generate_page,
 )
 from copy_source import (
@@ -9,7 +10,7 @@ def main():
     source = "./static"
     destination = "./public"
     clear_destination_and_copy_to(source, destination)
-    generate_page("content/index.md", "template.html", "public/index.html")
+    generate_pages_recursive("content/", "template.html", "public/")
 
 
 if __name__ == "__main__":
